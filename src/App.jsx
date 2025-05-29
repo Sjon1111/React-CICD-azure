c2f66e268b726b431ce4430b7ac66a61a4fc224f
Sjon1111 <sjon22557@gmail.com>
2025-05-30 01:49:53 +0530

    commit first

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React App for CI/CD-Azure</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Using Github Action CI/CD in azure devops
+        </p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
